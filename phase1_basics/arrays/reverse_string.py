420ee691ef35cbbef7ad4c48f77580497b52e890
Ayisha <ca20ayisharukshana.m@gmail.com>
2025-07-30 17:50:10 +0530

    updated

--- phase1_basics/arrays/reverse_string.py
+++ phase1_basics/arrays/reverse_string.py
@@ -4,6 +4,14 @@ Link: https://leetcode.com/problems/reverse-string/
 Approach: Two-pointer approach (in-place swap)
 Time Complexity: O(n) – where n is the number of characters
 Space Complexity: O(1) – no extra space used
+The reverse string in-place problem is not just about reversing — it's about teaching you:
+
+concepts to learn :
+
+Efficient memory handling
+Two-pointer logic
+Mutable vs immutable objects
+How to write functions that change arguments directly.
 """
 
 
@@ -15,5 +23,5 @@ class Solution:
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
-    
+   
    
